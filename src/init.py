abb7e5abf0324b722ce57614ba01a7c4d54141dd
chankane <brawnychocolate@gmail.com>
2021-05-04 03:00:11 +0900

    col.py

--- src/init.py
+++ src/init.py
@@ -1,21 +1,18 @@
 with open('./data/train.csv', 'r') as f:
     cols = f.readline().strip().split(',')
 
-# col.py
 with open('./src/col.py', 'r') as f:
     code = f.read()
 
-with open('./old/col.def.py', 'w') as f:
-    f.write(code)
-
+# 1
 a = []
 for col in cols:
     s = "def " + col + "(df):\n"
-    s += "    # any codes here...\n"
     s += "    return df\n"
     a.append(s)
 code = code.replace('# 1\n', '\n\n'.join(a))
 
+# 2
 s = ''
 for col in cols:
     s += "df = " + col + "(df)\n"
